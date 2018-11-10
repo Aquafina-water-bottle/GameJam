7ee7a784b4c9138138c0427881cdcedf6477f2e6
Coursaux <corso77@live.com>
2018-11-10 16:13:07 -0700

    background functionality

--- GameJamMain.py
+++ GameJamMain.py
@@ -1,5 +1,7 @@
 # import the pygame module, so you can use it
 import pygame
+import character
+import os
  
 # define a main function
 def main():
@@ -10,11 +12,19 @@ def main():
     pygame.display.set_caption("Escape The Village")
      
     # create a surface on screen that has the size of 240 x 180
-    screen = pygame.display.set_mode((500,500))
+    screen = pygame.display.set_mode((1920, 1080))
      
     # define a variable to control the main loop
     running = True
+    character_x = 1920/2
+    character_y = 1080/2
+    background = pygame.image.load(os.path.join('background-1.png'))
+    background = pygame.transform.scale(background, (840*4, 650*4))
+    background_x = 0
+    background_y = 0
      
+    clock = pygame.time.Clock()
+
     # main loop
     while running:
         # event handling, gets all event from the eventqueue
@@ -23,6 +33,19 @@ def main():
             if event.type == pygame.QUIT:
                 # change the value to False, to exit the main loop
                 running = False
+
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_UP]: background_y += 3
+        if pressed[pygame.K_DOWN]: background_y -= 3
+        if pressed[pygame.K_LEFT]: background_x += 3
+        if pressed[pygame.K_RIGHT]: background_x -= 3
+        
+        color = (255, 100, 0)
+        screen.fill((0, 0, 0))
+        screen.blit(background, (background_x, background_y))
+        pygame.draw.rect(screen, color, pygame.Rect(character_x, character_y, 60, 60))
+        pygame.display.flip()
+        clock.tick(60)
      
      
 # run the main function only if this module is executed as the main script
